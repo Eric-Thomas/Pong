c526459f119025354d48326f93429c415a7f3feb
Eric-Thomas <35507043+Eric-Thomas@users.noreply.github.com>
2018-07-18 15:56:16 -0400

    Add files via upload

--- pong.py
+++ pong.py
@@ -8,20 +8,33 @@ mainClock = pygame.time.Clock()
 # Create window
 screenWidth = 512
 screenHeight = 256
-DISPLAYSURF = pygame.display.set_mode((screenWidth,screenHeight))
+DISPLAYSURF = pygame.display.set_mode((screenWidth,screenHeight), 0, 32)
 pygame.display.set_caption("Pong")
 
-# Set color constants
+# Set constants
+FPS = 30
+fpsClock = pygame.time.Clock()
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 
 # Create Paddles
 paddleWidth = 10
 paddleHeight = 40
-leftPaddle = pygame.Rect(20, screenHeight/2 - paddleHeight/2, paddleWidth, paddleHeight)
-rightPaddle = pygame.Rect(screenWidth-paddleWidth - 20, screenHeight/2 - paddleHeight/2, paddleWidth, paddleHeight)
+leftPaddlex = 20
+leftPaddley = screenHeight/2 - paddleHeight/2
+rightPaddlex = screenWidth-paddleWidth - 20
+rightPaddley = screenHeight/2 - paddleHeight/2
+leftPaddle = pygame.Rect(leftPaddlex, leftPaddley, paddleWidth, paddleHeight)
+rightPaddle = pygame.Rect(rightPaddlex, rightPaddley, paddleWidth, paddleHeight)
 pygame.draw.rect(DISPLAYSURF, WHITE, leftPaddle)
 pygame.draw.rect(DISPLAYSURF,WHITE, rightPaddle)
+
+# Create Ball
+ballSize = 6
+ballx = screenWidth/2 - ballSize/2
+bally = screenHeight/2 - ballSize/2
+ball = pygame.Rect(ballx, bally, ballSize, ballSize)
+pygame.draw.rect(DISPLAYSURF, WHITE, ball)
 
 # Game loop
 while True:
@@ -29,5 +42,11 @@ while True:
 		if event.type == QUIT:
 			pygame.quit()
 			sys.exit()
-		pygame.display.update()
-
+		if event.type == MOUSEMOTION:
+			leftPaddle.move(leftPaddlex, event.pos[1])
+	ballx -= 1
+	pygame.draw.rect(DISPLAYSURF, WHITE, leftPaddle)
+	pygame.draw.rect(DISPLAYSURF, WHITE, rightPaddle)
+	pygame.draw.rect(DISPLAYSURF, WHITE, ball)
+	pygame.display.update()
+	fpsClock.tick(FPS)
